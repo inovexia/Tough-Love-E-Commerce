e07e0f0c3a898c342d3ac64fd36bb4f6cbb42ced
rajkumar-inovage <rajkumar@innovagesoftwares.com>
2021-01-06 18:52:50 +0530

    Signed-off-by: rajkumar-inovage <rajkumar@innovagesoftwares.com>

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,31 @@
 jQuery(document).ready(function () {
+  var swiper = new Swiper(".swiper", {
+    slidesPerView: 3,
+    spaceBetween: 0,
+
+    loop: true,
+    coverflow: {
+      rotate: 40,
+      stretch: 0,
+      depth: 50,
+      modifier: 1,
+      slideShadows: false,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 0,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 0,
+      },
+    },
+  });
   jQuery(".navbar-toggler-icon").click(function (event) {
     jQuery(".mob-menu").toggleClass("open");
     event.stopPropagation();
